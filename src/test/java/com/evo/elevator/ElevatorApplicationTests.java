3b4a603616c6064ded330913de27221844917e9b
Chisha Malama <chisha.malama@avenutech.com>
2022-04-20 17:47:29 +0200

    initial commit

---
+++ src/test/java/com/evo/elevator/ElevatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evo.elevator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
